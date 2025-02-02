0701ec23b85ed706347ad9f8b99e96d0ca207072
davye <davye@uw.edu>
2025-02-02 11:41:39 -0800

    parametric data manager/processor ckpt.

--- processing/parametric_data_manager.py
+++ processing/parametric_data_manager.py
@@ -35,7 +35,7 @@ class ParametricSHREDDataManager:
         self.reconstructor_data_elements = []
 
         self.input_summary = None
-        self.input_measurments = None
+        self.sensor_measurements = None
 
     #TODO: allow for sensor measurments as well
     def add(self, data, random_sensors = None, stationary_sensors = None, mobile_sensors = None, params=None, compression=None, scaling=None, time=None, id=None):
@@ -53,7 +53,7 @@ class ParametricSHREDDataManager:
         - scaling: scaling settings ('minmax', 'standard').
         - id: unique identifier for the dataset (string).
         """
-        data = get_data(data) # (n_traj, n_time, n_state)
+        # data = get_data(data) # (n_traj, n_time, n_state)
         compression = compression if compression is not None else self.compression
         scaling = scaling if scaling is not None else self.scaling
         time = time if time is not None else self.time
@@ -83,6 +83,16 @@ class ParametricSHREDDataManager:
                     method='random'
                 )
         self.reconstructor_data_elements.append(dataset_dict)
+        # print('data_processor.sensor_summary',data_processor.sensor_summary)
+        # print('data_processor.sensor_measurements_pd',data_processor.sensor_measurements_pd)
+        if data_processor.sensor_summary is not None and data_processor.sensor_measurements_pd is not None:
+            if self.input_summary is None and self.sensor_measurements is None:
+                self.input_summary = data_processor.sensor_summary
+                self.sensor_measurements = data_processor.sensor_measurements_pd
+            else:
+                self.input_summary = pd.concat([self.input_summary, data_processor.sensor_summary], axis = 0).reset_index(drop=True)
+                self.sensor_measurements = pd.merge(self.sensor_measurements, data_processor.sensor_measurements_pd, on=['time', 'trajectory'], how = 'inner')
+
         data_processor.discard_data()
         self.data_processors.append(data_processor)
         
@@ -124,9 +134,9 @@ class ParametricSHREDDataManager:
         X_valid_recon, y_valid_recon = None, None
         X_test_recon, y_test_recon = None, None
 
-        X_train_forecast, y_train_forecast = None, None
-        X_valid_forecast, y_valid_forecast = None, None
-        X_test_forecast, y_test_forecast = None, None
+        # X_train_forecast, y_train_forecast = None, None
+        # X_valid_forecast, y_valid_forecast = None, None
+        # X_test_forecast, y_test_forecast = None, None
 
         # Process reconstructor datasets
         for dataset_dict in self.reconstructor_data_elements:
@@ -172,3 +182,79 @@ class ParametricSHREDDataManager:
         SHRED_test_dataset = SHREDDataset(test_recon_dataset)
 
         return SHRED_train_dataset, SHRED_valid_dataset, SHRED_test_dataset
+    
+
+
+
+    def postprocess(self, data, uncompress = None, unscale = None):
+        uncompress = uncompress if uncompress is not None else self.compression is not None
+        unscale = unscale if unscale is not None else self.scaling == "minmax" # prob change to boolean
+        results = {}
+        start_index = 0
+        for data_processor in self.data_processors:
+            field_spatial_dim = data_processor.Y_spatial_dim
+            print('field_spatial_dim',field_spatial_dim)
+            field_data = data[:, start_index:start_index+field_spatial_dim]
+            if isinstance(data, torch.Tensor):
+                field_data = field_data.cpu().numpy()
+            start_index = field_spatial_dim + start_index
+            print('field_data.shape',field_data.shape)
+            field_data = data_processor.inverse_transform(field_data, uncompress, unscale)
+            results[data_processor.id] = field_data
+        return results
+    
+
+    def generate_X(self, measurements, params):
+        # n_traj, n_times, nsensors
+        # n_traj, n_times, nparams
+        # for each field, concatenate the appropriate measurements with the appropriate params
+        # seperate out params measurements from sensor measurements, so it is clear the order of params and which belong to which dataset
+        results = None
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        start_sensor = 0
+        for data_processor in self.data_processors:
+            end_sensor = start_sensor + data_processor.sensor_measurements.shape[1]
+            field_measurements = measurements[:,start_sensor:end_sensor]
+            result = data_processor.generate_X_New(field_measurements) # (n_traj, n_time, n_sensors + n_params)
+
+
+
+        results = torch.tensor(results, dtype=torch.float32, device=device)
+        return results
+        # start_sensor = 0
+        # if start is None and end is None:
+        #     # generate lagged sequences from measurements
+        #     for data_processor in self.data_processors:
+        #         end_sensor = start_sensor + data_processor.sensor_measurements.shape[1]
+        #         result = data_processor.transform_X(measurements[:,start_sensor:end_sensor])
+        #         if results is None:
+        #             results = result
+        #         else:
+        #             results = np.concatenate((results, result), axis = 1)
+        #         start_sensor = end_sensor
+        #     results = generate_lagged_sequences_from_sensor_measurements(results, self.lags)
+        # else:
+        #     # generate lagged sequences from start to end (inclusive)
+        #     for data_processor in self.data_processors:
+        #         if data_processor.sensor_measurements is not None:
+        #             end_sensor = start_sensor + data_processor.sensor_measurements.shape[1]
+        #             if measurements is not None:
+        #                 field_measurements = measurements[:,start_sensor:end_sensor]
+        #                 result = data_processor.generate_X(end = end, measurements = field_measurements, time = time)
+        #             else:
+        #                 result = data_processor.generate_X(end = end, measurements = None, time = time)
+        #             if results is None:
+        #                 results = result
+        #             else:
+        #                 results = np.concatenate((results, result), axis = 1)
+        #             start_sensor = end_sensor
+        #     # extract indices without data (gaps)
+        #     gap_indices = np.where(np.isnan(results).any(axis=1))[0]
+        #     for gap in gap_indices:
+        #         # gap is being forecasted, gap - 1 is 'current'
+        #         gap_lagged_sequence = results[gap - 1 - self.lags:gap,:].copy()
+        #         gap_lagged_sequence = gap_lagged_sequence[np.newaxis,:,:]
+        #         gap_lagged_sequence = torch.tensor(gap_lagged_sequence, dtype=torch.float32, device=device)
+        #         results[gap] = forecaster(gap_lagged_sequence).detach().numpy()
+        #     results = generate_lagged_sequences_from_sensor_measurements(results, self.lags)
+        #     results = results[start:end+1,:,:]
--- processing/parametric_data_processor.py
+++ processing/parametric_data_processor.py
@@ -31,7 +31,14 @@ class ParametricSHREDDataProcessor:
         - params: dataset of parameters of shape (nparams, ntimes), time on last axis just like with data. (string or numpy array)
         - id: unique identifier for the dataset (string).
         """
-        self.data = data
+        if compression is not None:
+            if compression == True:
+                self.n_components = 20
+            elif isinstance(compression, int):
+                self.n_components = compression
+            else:
+                raise ValueError('Compression must be a bool or an int.')
+        self.data = get_data(data)
         self.ntrajectories = self.data.shape[0]
         self.ntimes = len(time)
         if params is not None:
@@ -40,7 +47,7 @@ class ParametricSHREDDataProcessor:
             self.nparams = 0
         # self.data = get_data(data) # full-state data where the last axis is time
         self.n_components = compression # DAVID THIS IS FOR TESTING ONLY TODO: add processing/validation
-        self.full_state_data = None
+        # self.full_state_data = None
         self.params = params
 
         self.random_sensors = random_sensors # number of randomly placed sensors
@@ -58,12 +65,16 @@ class ParametricSHREDDataProcessor:
         self.right_singular_values = {}
         self.scaler = {} # stores scaler of full-state data
         self.transformed_data = {}
-        self.original_shape = self.data.shape
-        self.train_indices = None
-        self.val_indices = None
-        self.test_indices = None
+        # self.original_shape = self.data.shape
+
         self.sensor_measurements = None # sensor measurements of shape (n_sensors, t), time on last axis
         self.sensor_summary = None # summary of sensor locations
+        self.sensor_summary = None
+        self.sensor_measurements = None
+        self.sensor_measurements_pd = None
+        self.id = str(id)
+        self.Y_spatial_dim = None
+        self.random_sensor_locations = None
 
     # TODO: Move self.full_state_data from __init__ to a method called add or something
     # adding the very first trajectory/data should be no different from adding a
@@ -100,47 +111,69 @@ class ParametricSHREDDataProcessor:
         """
         Sets train, validation, and test SHREDDataset objects with generated dataset.
         """
-        # saves indices as attributes for adding new trajectory
-        self.train_indices = train_indices
-        self.val_indices = val_indices
-        self.test_indices = test_indices
-        # print('train indices', train_indices)
-        # print('val_indices', val_indices)
-        # print('test_indices', test_indices)
-
         X_train, X_valid, X_test = None, None, None
         y_train, y_valid, y_test = None, None, None
 
         # Processing regarding X
-
         # Parametric Case:
-
-
         all_traj_sensor_measurements = None
+
         for i in range(self.data.shape[0]):
             single_traj_data = self.data[i]
-            sensor_measurements_dict = get_sensor_measurements(single_traj_data, self.random_sensors, self.stationary_sensors, self.mobile_sensors) # get sensor data
-            # Add a new axis to array2 to match dimensions
-            sensor_measurements_dict['sensor_measurements'] = sensor_measurements_dict['sensor_measurements'][np.newaxis, :, :]  # add trajectory axis
-            # print("sensor_measurements_dict['sensor_measurements'].shape:",sensor_measurements_dict['sensor_measurements'].shape)
-            if all_traj_sensor_measurements is None:
-                all_traj_sensor_measurements = sensor_measurements_dict['sensor_measurements']
-                self.nsensors = all_traj_sensor_measurements.shape[2]
-            else:
-                all_traj_sensor_measurements = np.concatenate((all_traj_sensor_measurements, sensor_measurements_dict['sensor_measurements']), axis = 0)
-            self.sensor_summary = sensor_measurements_dict['sensor_summary']
 
-            if self.sensor_summary is None:
-                self.sensor_summary = sensor_measurements_dict['sensor_summary']
-        
+                
+            if i == 0:
+                if self.random_sensors is not None:
+                    if isinstance(self.random_sensors, int):
+                        self.random_sensor_locations = generate_random_sensor_locations(full_state = single_traj_data, num_sensors = self.random_sensors)
+            
+                self.stationary_sensors = self.stationary_sensors + self.random_sensor_locations
+            sensor_measurements_dict = get_sensor_measurements(full_state_data=single_traj_data,
+                                                               id = self.id, time = self.time,
+                                                               stationary_sensors=self.stationary_sensors,
+                                                               mobile_sensors=self.mobile_sensors,
+                                                               random_sensors=None) # get sensor data
+            if sensor_measurements_dict['sensor_measurements'] is not None:
+                sensor_measurements = sensor_measurements_dict['sensor_measurements'].drop(columns=['time']).to_numpy()[np.newaxis, :] # add trajectory axis
+                sensor_measurements_pd = sensor_measurements_dict['sensor_measurements']
+                sensor_measurements_pd.insert(0, "trajectory", i)
+                if self.params is not None:
+                    single_param_data = self.params[i]
+                    single_param_data_df = pd.DataFrame(single_param_data, columns=[f"{self.id} param {i}" for i in range(single_param_data.shape[1])])
+                    sensor_measurements_pd = pd.concat([sensor_measurements_pd, single_param_data_df], axis=1)
+                sensor_summary = sensor_measurements_dict['sensor_summary']
+    
+                
+                # sensor_measurements_dict['sensor_measurements'] = sensor_measurements_dict['sensor_measurements'].drop(columns=['time']).to_numpy()
+                # sensor_measurements_dict['sensor_measurements'] = sensor_measurements_dict['sensor_measurements'][np.newaxis, :, :]  # add trajectory axis
+                # self.sensor_summary = sensor_measurements_dict['sensor_summary']
+
+                # Add a new axis to array2 to match dimensions
+                # sensor_measurements_dict['sensor_measurements'] = sensor_measurements_dict['sensor_measurements'][np.newaxis, :, :]  # add trajectory axis
+                # print("sensor_measurements_dict['sensor_measurements'].shape:",sensor_measurements_dict['sensor_measurements'].shape)
+                if all_traj_sensor_measurements is None:
+                    all_traj_sensor_measurements = sensor_measurements
+                    self.nsensors = all_traj_sensor_measurements.shape[2]
+                else:
+                    all_traj_sensor_measurements = np.concatenate((all_traj_sensor_measurements, sensor_measurements), axis = 0)
+                if self.sensor_summary is None:
+                    self.sensor_summary = sensor_summary
+                # print('sensor_measurements_pd', sensor_measurements_pd)
+                if self.sensor_measurements_pd is None:
+                    self.sensor_measurements_pd = sensor_measurements_pd
+                else:
+                    self.sensor_measurements_pd = pd.concat([self.sensor_measurements_pd, sensor_measurements_pd], axis=0, ignore_index=True)
+                
+
+
 
         # print('all_traj_sensor_measurements', all_traj_sensor_measurements.shape)
-        print('sensor summary', self.sensor_summary)
+        # print('sensor summary', self.sensor_summary)
             # else:
             #     self.sensor_summary = pd.concat([self.sensor_summary, sensor_measurements_dict['sensor_summary']], 
             #                                     axis = 0).reset_index(drop=True)
         # check if sensor_measurements exist
-        if all_traj_sensor_measurements.size != 0:
+        if all_traj_sensor_measurements is not None:
             if self.params is not None:
                 all_traj_sensor_measurements_and_params = np.concatenate((all_traj_sensor_measurements, self.params), axis=2)  # Shape (4, 2000, 3)
             else:
@@ -148,7 +181,7 @@ class ParametricSHREDDataProcessor:
             train_traj_sensor_measurements_and_params = all_traj_sensor_measurements_and_params[train_indices]
             valid_traj_sensor_measurements_and_params = all_traj_sensor_measurements_and_params[val_indices]
             test_traj_sensor_measurements_and_params = all_traj_sensor_measurements_and_params[test_indices]
-            print(train_traj_sensor_measurements_and_params.shape[2])
+            # print(train_traj_sensor_measurements_and_params.shape[2])
             flattened_train_traj_sensor_measurements_and_params = train_traj_sensor_measurements_and_params.reshape(
                 train_traj_sensor_measurements_and_params.shape[0] * train_traj_sensor_measurements_and_params.shape[1],
                 train_traj_sensor_measurements_and_params.shape[2])
@@ -226,10 +259,12 @@ class ParametricSHREDDataProcessor:
         self.fit(train_data_flattened)
         # transform
         y_train, y_valid, y_test = self.transform(train_data_flattened, valid_data_flattened, test_data_flattened)
+        self.Y_spatial_dim = y_train.shape[1]
         # generate y data
         # y_train, y_valid, y_test = self.generate_y(train_indices, val_indices, test_indices, method)
         self.full_state_data = None
         print('done generating dataset')
+        # print('self.sensor_measurements_pd',self.sensor_measurements_pd)
         # full_state_data = self.unflatten(full_state_data)
         if X_train is not None and X_valid is not None and X_test is not None: # aka make sure sensor data exists, does not work for setting train, validation, and test to None/0 yet
             return {
@@ -426,3 +461,48 @@ class ParametricSHREDDataProcessor:
         self.transformed_data = None
         self.transformed_sensor_data = None
 
+
+    def inverse_transform(self, data, uncompress, unscale):
+        """
+        Expects data to be a np array with time on axis 0.
+        (Field specific output fron SHRED/Reconstructor)
+        """
+        # check if scaler fitted on reconstructor is not None
+        if self.scaler is not None and unscale is True:
+            data = self.scaler.inverse_transform(data)
+        # check if compression is None
+        if self.right_singular_values is not None and uncompress is True:
+            data = data @ self.right_singular_values
+        return data
+    
+
+    def generate_X_New(self,data):
+        # (n_traj, n_time, n_sensors + n_params)
+        # need to pass in the first lags number of data set well though
+        lagged_sequences = None
+        for i in range(data.shape[0]):
+            data = data[i]
+            if lagged_sequences is None:
+                lagged_sequences = generate_lagged_sequences_from_sensor_measurements(data, self.lags)
+            else:
+                lagged_sequences = np.concatenate((lagged_sequences, generate_lagged_sequences_from_sensor_measurements(data, self.lags)), 0)
+
+        # timesteps = end+1 # end_time inclusive
+        # nsensors = self.sensor_measurements.shape[1] # make sure measurements dim matches
+        # complete_measurements = np.full((timesteps, nsensors), np.nan)
+        # if timesteps > len(self.sensor_measurements):
+        #     print('hello')
+        #     complete_measurements[0:len(self.sensor_measurements),:] = self.sensor_measurements
+        # else:
+        #     print('bye')
+        #     complete_measurements[0:timesteps,:] = self.sensor_measurements[0:timesteps,:]
+        # if measurements is not None and time is not None:
+        #     for i in range(len(time)):
+        #         if time[i] < complete_measurements.shape[0]:
+        #             complete_measurements[time[i],:] = measurements[i,:]
+        complete_measurements = self.sensor_scaler['random'].transform(complete_measurements)
+        return complete_measurements
+
+    # used for transforming raw new sensor measurements
+    def transform_X(self, measurements):
+        return self.sensor_scaler['random'].transform(measurements)
