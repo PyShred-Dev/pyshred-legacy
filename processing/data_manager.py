56e6da64bdf127f2979921b54835c96d5aae137e
davye <davye@uw.edu>
2025-02-08 01:15:05 -0800

    update generate_X

--- processing/data_manager.py
+++ processing/data_manager.py
@@ -340,7 +340,7 @@ class SHREDDataManager:
                 results[gap_indices] = 0
             results_sensor_measurements = self.postprocess_sensor_measurements(data = results, method = 'sensor_forecaster')
             results_sensor_measurements = np.concatenate((np.zeros((self.lags, results_sensor_measurements.shape[1])), results_sensor_measurements), axis = 0)
-            results_sensor_measurements = results[start:end+self.lags+1,:,:]
+            results_sensor_measurements = results[start:end+self.lags+1,:]
             results = generate_lagged_sequences_from_sensor_measurements(results, self.lags)
             results = results[start:end+1,:,:]
         results = torch.tensor(results, dtype=torch.float32, device=device)
