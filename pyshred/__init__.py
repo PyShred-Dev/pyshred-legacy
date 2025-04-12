a3381941b91c808d8af405ac93f9c1376f845cbb
davye <davye@uw.edu>
2025-04-12 12:52:44 -0700

    basic functionality checkpoint

--- pyshred/__init__.py
+++ pyshred/__init__.py
@@ -1,8 +1,9 @@
 from .models.shred_models import SHRED
+from .models.shred_models import SINDySHRED
 from .models.sequence_models import LSTM
 from .models.decoder_models import SDN
 from .processing.data_manager import SHREDDataManager
 from .processing.parametric_data_manager import ParametricSHREDDataManager
 from .processing.utils import evaluate
 
-__all__ = ["SHRED", "LSTM", "SDN", "SHREDDataManager", "ParametricSHREDDataManager", "evaluate"]
+__all__ = ["SHRED", "SINDySHRED", "LSTM", "SDN", "SHREDDataManager", "ParametricSHREDDataManager", "evaluate"]
--- pyshred/models/shred_models/__init__.py
+++ pyshred/models/shred_models/__init__.py
@@ -1,3 +1,5 @@
 from .shred import SHRED
+from .sindy_shred import SINDySHRED
 
-__all__ = ["SHRED"]
+
+__all__ = ["SHRED", "SINDySHRED"]
